3849b227f02314fbd3dca26959402e8142c25f0b
Aaddityaaa <adityau5090@gmail.com>
2026-03-05 17:54:18 +0530

    add cors in server

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const { authRouter } = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 app.use("/api/auth",authRouter);
 
