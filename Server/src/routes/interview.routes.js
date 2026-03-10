9b986c22e9ff0181cfdc52fd9dc14c48eafc6988
Aaddityaaa <adityau5090@gmail.com>
2026-03-10 14:49:10 +0530

    add multer middleware for taking resume

--- Server/src/routes/interview.routes.js
+++ Server/src/routes/interview.routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const { authUser } = require("../middleware/auth.middleware");
 const { generateInterviewReportController } = require("../controller/interview.controller");
+const { upload } = require("../middleware/file.middleware");
 const interviewRouter = express.Router();
 
-interviewRouter.post("/",authUser, generateInterviewReportController)
+interviewRouter.post("/",authUser, upload.single("resume"), generateInterviewReportController)
 
 module.exports = { interviewRouter }
